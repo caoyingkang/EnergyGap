4485df33229364485e4f75b56e70cf441ca932c8
Yingkang Cao <cyngkng@gmail.com>
2025-12-09 22:34:35 -0500

    move utils into the main scripts

--- src/run_ed.jl
+++ src/run_ed.jl
@@ -2,9 +2,47 @@
 using DataFrames
 using CSV
 using KrylovKit: eigsolve
+using QuantumToolbox: sigmax, sigmaz, multisite_operator
 
-include("ed_utils.jl")
+const X = sigmax()
+const Z = sigmaz()
 
+
+"""
+Build the Hamiltonian for the [[6k, 2k, 2]] code described in https://arxiv.org/abs/1911.01354.
+Add to the Hamiltonian extra penalty terms with strength `g` (default 1.0) to lift the ground space degeneracy.
+
+The sites of the lattice are indexed in this way:
+
+1---ZZ---4---ZZ--- ... ---3⋅(2k)-2
+|        |                 |
+XX+gZZ  XX+gZZ            XX+gZZ
+|        |                 |
+2        5         ...    3⋅(2k)-1
+|        |                 |
+ZZ       ZZ                ZZ
+|        |                 |
+3---XX---6---XX--- ... ---3⋅(2k)
+"""
+function build_hamiltonian(::Val{K}; g::Float64=1.0) where {K}
+    m = 2 * K  # number of rungs
+    # horizontal couplings
+    H = sum(
+        multisite_operator(Val(6K), 3 * j - 2 => Z, 3 * (j + 1) - 2 => Z) +
+        multisite_operator(Val(6K), 3 * j => X, 3 * (j + 1) => X)
+        for j in 1:(m-1)
+    )
+    # vertical couplings
+    H += sum(
+        g * multisite_operator(Val(6K), 3 * j - 2 => Z, 3 * j - 1 => Z) +
+        multisite_operator(Val(6K), 3 * j - 2 => X, 3 * j - 1 => X) +
+        multisite_operator(Val(6K), 3 * j - 1 => Z, 3 * j => Z)
+        for j in 1:m
+    )
+    return H.data
+end
+
+k_values = 1:3
 
 results_dir = joinpath(@__DIR__, "results")
 mkpath(results_dir)
@@ -12,9 +50,8 @@ csv_path = joinpath(results_dir, "ED_energy_gap_vs_k.csv")
 
 
 ## --------------------------- Run ED calculation ------------------------------------
-
 df = DataFrame(k=Int[], E0=Float64[], E1=Float64[], ΔE=Float64[], log10_ΔE=Float64[])
-for k in 1:3
+for k in k_values
     println("\nComputing for k = $k (total sites: $(6*k))...")
     H = build_hamiltonian(Val(k))
     Es, ψs, info = eigsolve(H, 2, :SR; ishermitian=true, verbosity=3)
